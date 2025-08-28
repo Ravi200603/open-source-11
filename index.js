137d136b24881600ca24dad08959e20c186d3f2c
Ravinder Singh <T00725333@mytru.ca>
2025-08-28 02:35:48 -0700

    my first open source experience

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("LMAO")
+
+
+console.log("first commit in open source nothing important just wanted to try something in which I download the code form github and add something into it ");
